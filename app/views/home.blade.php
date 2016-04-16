05630f083b4d4034550b0c04ff326855d068dca7
Lourence <Lourence@Lourence-PC>
2016-04-16 00:31:18 -0700

    New navbar

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,24 +3,50 @@
     <head>
         <link rel="stylesheet" type="text/css" href="{{ asset('materialize/css/materialize.css') }}"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <script src="{{ asset('js/jquery.js')}}"></script>
     </head>
     <body>
-        <h1>This is a material design </h1>
-        <div>
-            <a class="btn" onclick="Materialize.toast('I am a toast', 4000)">Toast!</a>
-        </div>
-        <div>
-            <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Drop Me!</a>
-            <!-- Dropdown Structure -->
-            <ul id='dropdown1' class='dropdown-content'>
-              <li><a href="#!">one</a></li>
-              <li><a href="#!">two</a></li>
-              <li class="divider"></li>
-              <li><a href="#!">three</a></li>
+        <nav class="light-blue lighten-1" role="navigation">
+        <div class="nav-wrapper">
+            <a href="#!" class="brand-logo" style="margin-left: 10px;">MaidFinder PH</a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li>
+                    <form>
+                        <div class="input-field">
+                            <input id="search" type="search" required>
+                            <label for="search"><i class="material-icons">search</i></label>
+                            <i class="material-icons">close</i>
+                        </div>
+                    </form>
+                </li>
+                <li>
+                    <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Sign up</a>
+                        <!-- Dropdown Structure -->
+                        <ul id='dropdown1' class='dropdown-content'>
+                            <li><a>As an</a></li>
+                            <li class="divider"></li>
+                          <li><a href="#!">Employeer</a></li>
+                          <li><a href="#!">Applicant</a></li>
+                        </ul>
+                </li>
+                <li><a href="#">Login</a></li>
+            </ul>
+            <ul class="side-nav" id="mobile-demo">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
             </ul>
         </div>
+    </nav>
+       @yield('content')
     </body>
     <script src="{{ asset('js/jquery.js')}}"></script>
     <script src="{{ asset('materialize/js/materialize.min.js') }}"></script>
+    <script>
+        $(".button-collapse").sideNav();
+    </script>
 <html>
     
