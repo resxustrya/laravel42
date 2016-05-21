69c2adc7438eeb18852db52020f427d2ecc31f39
rexustraya <rexustraya@gmail.com>
2016-05-21 16:14:17 +0800

    New Update codes

---
+++ app/controllers/TableController.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Hp
+ * Date: 5/17/2016
+ * Time: 9:18 PM
+ */
+class TableController
+{
+
+}
---
+++ app/views/card.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hp
+ * Date: 5/17/2016
+ * Time: 9:35 PM
+ */
